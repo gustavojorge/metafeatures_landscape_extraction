14fe65504460ae04563a403dbfd404290212f6b7
João Gabriel <jg.lofiego@gmail.com>
2025-02-14 17:56:21 -0300

    Change shell scripts to vary on r instead of l

--- scripts/smaller_scripts/adaptative_walk_decomposition.sh
+++ scripts/smaller_scripts/adaptative_walk_decomposition.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 r=("0.05" "0.1" "0.25" "0.5" "1.0")
-l=("5" "10" "25" "50" "100")
+L="0"
 
 instance="${1:-A}"
 
-for L in "${l[@]}"; do
-    echo "Executing adaptative_walk_decomposition with: $instance $L"
+for R in "${r[@]}"; do
+    echo "Executing adaptative_walk_decomposition with: $instance $R"
 
-    log_file="../dataset/$instance/decomposition_based/adaptative_walk/l${L}/log.txt"
+    log_file="../dataset/$instance/decomposition_based/adaptative_walk/r${R}/log.txt"
         
     {
         echo "============================== LOG =============================="
 
-        { time ./exe/adaptative_walk_decomposition "$instance" "$L"; } 2>&1
+        { time ./exe/adaptative_walk_decomposition "$instance" "$L" "$R"; } 2>&1
 
         echo "================================================================="
     } > "$log_file" 2>&1
--- scripts/smaller_scripts/adaptative_walk_pareto.sh
+++ scripts/smaller_scripts/adaptative_walk_pareto.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 r=("0.05" "0.1" "0.25" "0.5" "1.0")
-l=("5" "10" "25" "50" "100")
+L="0"
 
 instance="${1:-A}"
 
-for L in "${l[@]}"; do
-    echo "Executing adaptative_walk_pareto with: $instance $L"
+for R in "${r[@]}"; do
+    echo "Executing adaptative_walk_pareto with: $instance $R"
 
-    log_file="../dataset/$instance/pareto_based/adaptative_walk/l${L}/log.txt"
+    log_file="../dataset/$instance/pareto_based/adaptative_walk/r${R}/log.txt"
         
     {
         echo "============================== LOG =============================="
 
-        { time ./exe/adaptative_walk_pareto "$instance" "$L"; } 2>&1
+        { time ./exe/adaptative_walk_pareto "$instance" "$L" "$R"; } 2>&1
 
         echo "================================================================="
     } > "$log_file" 2>&1
